cc97592cdbda62f9c33226755be846d72dfc426f
Yieison <yemarlipe@gmail.com>
2023-12-06 22:15:56 -0500

    Se crean los controladores REST

---
+++ src/main/java/com/pw/TiendaRopa/controller/ProductoController.java
@@ -0,0 +1,45 @@
+package com.pw.TiendaRopa.controller;
+
+import com.pw.TiendaRopa.model.Producto;
+import com.pw.TiendaRopa.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public List<Producto> listarProductos() {
+        return productoService.listarProductos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable Long id) {
+        return productoService.obtenerProductoPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Producto agregarProducto(@RequestBody Producto producto) {
+        return productoService.guardarProducto(producto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Producto> actualizarProducto(@PathVariable Long id, @RequestBody Producto producto) {
+        return ResponseEntity.ok(productoService.actualizarProducto(id, producto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarProducto(@PathVariable Long id) {
+        productoService.eliminarProducto(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/pw/TiendaRopa/controller/UsuarioController.java
@@ -0,0 +1,45 @@
+package com.pw.TiendaRopa.controller;
+
+import com.pw.TiendaRopa.model.Usuario;
+import com.pw.TiendaRopa.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+        return usuarioService.listarUsuarios();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable Long id) {
+        return usuarioService.obtenerUsuarioPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Usuario registrarUsuario(@RequestBody Usuario usuario) {
+        return usuarioService.guardarUsuario(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> actualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        return ResponseEntity.ok(usuarioService.actualizarUsuario(id, usuario));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarUsuario(@PathVariable Long id) {
+        usuarioService.eliminarUsuario(id);
+        return ResponseEntity.ok().build();
+    }
+}
